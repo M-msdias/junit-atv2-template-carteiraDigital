fe6847af81784ca9282f2818845334d3a27aa36a
M-msdias <ddiasmarcela@gmail.com>
2025-09-14 21:05:50 +0000

    Atividade concluída

--- src/test/java/Deposito.java
+++ src/test/java/Deposito.java
@@ -8,13 +8,18 @@ import com.example.DigitalWallet;
 
 
 class Deposito {
-        
+        @ParameterizedTest
+        @ValueSource(doubles = {0.01, 10.0, 20.5, 999.99})
         void deveDepositarValoresValidos(double amount) {
-            
+            DigitalWallet digitalWallet = new DigitalWallet("Bob", 0);
+            digitalWallet.deposit(amount);
+            assertEquals(digitalWallet.getBalance(), amount, 0.0001, "O valor para depositar tem que ser válido");
         }
 
-        
+        @ParameterizedTest
+        @ValueSource(doubles = {0, -10, -20})
         void deveLancarExcecaoParaDepositoInvalido(double amount) {
-            
+            DigitalWallet digitalWallet = new DigitalWallet("Bob", 0.0);
+            assertThrows(IllegalArgumentException.class, () -> digitalWallet.deposit(amount), "Depósito com valor menor igual a zero deve lançar exceção");
         }
     }
--- src/test/java/SaldoInicial.java
+++ src/test/java/SaldoInicial.java
@@ -6,11 +6,17 @@ import org.junit.jupiter.api.Test;
 import com.example.DigitalWallet;
 
 class SaldoInicial {
-        void deveConfigurarSaldoInicialCorreto() {
-            
-        }
+        DigitalWallet digitalWallet;
+    @Test
+    void deveConfigurarSaldoInicialCorreto() {
+        digitalWallet = new DigitalWallet("Bob",50);
+        assertEquals ( 50, digitalWallet.getBalance());
+    }
 
+     @Test
         void deveLancarExcecaoParaSaldoInicialNegativo() {
-            
+            assertThrows(IllegalArgumentException.class, () -> new DigitalWallet("Bob", -30));
+    
+           
         }
     }
